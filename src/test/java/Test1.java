5b675f0d2326e4532b6fa353abb8447fc78acd64
Tanya Andreeva <andreeva-tanja@mail.ru>
2016-07-02 18:26:41 +0300

    test #1

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,39 +1,64 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
 
-import static org.testng.AssertJUnit.assertTrue;
 
 public class Test1 {
-    private WebDriver driver;
+    private void openStatement() throws InterruptedException {
+        open("https://rencredit.ru");
+        AssertJUnit.assertTrue(title().contains("Ренессанс Кредит"));
+    }
 
-    @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+    private void gotoInvestorsPage()throws InterruptedException {
+        $(By.linkText("О банке")).click();
+        $(By.linkText("Инвесторам")).click();
+        $(By.linkText("Отчетность")).click();
+        $(By.linkText("Отчетность банка")).click();
+        TimeUnit.SECONDS.sleep(1);
+    }
 
+    private void saveLogo() throws IOException, InterruptedException {
+        URL url = new URL($(By.xpath("//img[@class='logo__image']")).getAttribute("src"));
+        BufferedImage bufferedImage = ImageIO.read(url);
+        ImageIO.write(bufferedImage, "png", new File("logo.png"));
+        System.out.println("Image downloaded");
+        TimeUnit.SECONDS.sleep(1);
+    }
+
+    private void savePDF() throws IOException, InterruptedException{
+        System.out.println("Opening connection");
+        URL url1 = new URL("https://rencredit.ru/upload/iblock/5dc/msfo_otchetnost_renkredit_2015_1a.pdf");
+        InputStream in = url1.openStream();
+        FileOutputStream fos = new FileOutputStream(new File("pdfFile.pdf"));
+
+        System.out.println("Reading from resource and writing to file");
+        int length;
+        byte[] buffer = new byte[1024];
+        while ((length = in.read(buffer)) > -1)
+            fos.write(buffer, 0, length);
+
+        fos.close();
+        in.close();
+        System.out.println("File downloaded");
+
+        TimeUnit.SECONDS.sleep(1);
     }
 
     @Test
-    public void gotoRencreditPage() {
-        driver.get("https://rencredit.ru");
-        assertTrue(driver.getTitle().contains("Ренессанс Кредит"));
+    public void test1() throws Exception {
+        openStatement();
+        saveLogo();
+        gotoInvestorsPage();
+        savePDF();
     }
 
-    public void saveLogo()
-    {
-        //найти, как сохранить логитип сайта
-        //текст
-    }
-
-    @Test
-    public void gotoInvestorsPage(){
-        driver.get("https://rencredit.ru/investors/reporting/");
-        //найти, как сохранить pdf
-
-
-    }
 
 }
